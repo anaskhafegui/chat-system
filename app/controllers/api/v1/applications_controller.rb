b7fc80a566939f230f2b3ddf60378c2366a7ac1c
Anas Khfagy <anas.mdhat@outlook.com>
2022-01-12 17:27:07 +0200

    validate authenticate application controller by token

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -1,23 +1,8 @@
 module Api
     module V1
         class ApplicationsController < ApplicationController 
-            before_action :authorization
-
-            def authorization!
-                authenticate_with_http_token do |token, options|
-                  @current_user = User.find_by(:auth_token => token)
-                end
-            
-                unless @user.present?
-                  # You could return anything you want if the response if it's unauthorized. in this
-                  # case I'll just return a json object 
-                  return render json: {
-                    status: 300,
-                    message: "Unauthorized access in the API"
-                  }, status: 401
-                end
-              end
-            end
+            # authenticate_token by token exepct app index for testing
+            before_action :require_login!, except: [:index]
             
             def index
                 applications = Application.order('created_at DESC');
@@ -49,14 +34,27 @@ module Api
                 render json: {status: 'SUCCESS', message: 'Loaded Application Chats', data:chats }, status: :ok
             end
             
+            private 
+            
+                def application_params
+                    params.permit(:name)
+                end
 
-            private def application_params
-                params.permit(:name)
-            end
+                def get_application_by_token
+                    return Application.find_by(token: params[:token]);
+                end
 
-            private def get_application_by_token
-                return Application.find_by(token: params[:token]);
-            end
+                def require_login!
+                    authenticate_token || render_unauthorized('Access Denied: Unauthorized Access')
+                end
+
+                def authenticate_token
+                    get_application_by_token
+                end  
+
+                def render_unauthorized(message)
+                    render json: { status: 'Failed' , message: 'Unauthorized Token' ,data: message}, status: :unprocessable_entity    
+                end
         end
     end
 end
